16b32de3600ba82e139b8e93e0e7ba8ed7d29ee7
root <vaidehi.venkatesan@gmail.com>
2013-09-12 09:51:06 -0400

    Modified crawler to get contents from website instead of mobile version

--- cuisineCrawler/getEatYourBooks.py
+++ cuisineCrawler/getEatYourBooks.py
@@ -1,6 +1,7 @@
-import urllib.request
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 import re
-from HTMLParser import HTMLParser
+from html.parser import HTMLParser
 
 
 class Downloader():
@@ -13,8 +14,8 @@ class Downloader():
 		self.contents = ''
 
 	def download(self):
-		request = urllib.request.Request(self.url)
-		try: response = urllib.request.urlopen(request)
+		req = Request(self.url)
+		try: response = urlopen(req)
 		except HTTPError as e:
 			print('The server couldn\'t fulfill the request.')
 			print('Error code: ', e.code)
@@ -26,12 +27,11 @@ class Downloader():
 				print('Server couldn\'t fullfill the request.')
 				print('Error code: ', e.code)
 		else:
-			if response == 200:
-				self.contents = response.read()
-				f = open('urlContents.txt','w')
-				f.write(self.contents)
-			else:
-				print "Invalid response: ", response
+			contents = response.read()
+			self.contents = contents.decode(encoding='UTF-8')
+			f = open('urlContents.txt','w')
+			f.write(self.contents)
+			
 
 class EatYourBooksParser(HTMLParser):
 	'''
@@ -41,15 +41,15 @@ class EatYourBooksParser(HTMLParser):
 
 	def handle_starttag(self, tag, attrs):
 		if tag.find('li',0,tag.__len__()-1) >= 0:
-			print "Encountered a start tag:", tag
+			print ('Encountered a start tag: ', tag)
 	
 	def handle_endtag(self, tag):
 		if tag.find('li',0,tag.__len__()-1) >= 0:
-			print "Encountered an end tag:", tag
+			print ('Encountered an end tag: ', tag)
 	
 	def handle_data(self, data):
 		if data.find('li',0,data.__len__()-1) >= 0:
-			print "Encountered some data:", data
+			print ('Encountered some data: ', data)
 
 
 if __name__== '__main__':
