859debd090702e3909eccdedcff2130f31f377d6
Vaidehi <vaidehi@vaidehi-M51AC.(none)>
2013-12-29 14:49:46 -0500

    Adding HTML5 code for Coquere

---
+++ coquere/js/skel-tabs.min.js
@@ -0,0 +1,13 @@
+$(function(){
+  $('ul.tabs li:first').addClass('active');
+  $('.block article').hide();
+  $('.block article:first').show();
+  $('ul.tabs li').on('click',function(){
+    $('ul.tabs li').removeClass('active');
+    $(this).addClass('active')
+    $('.block article').hide();
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).show();
+    return false;
+  });
+})
