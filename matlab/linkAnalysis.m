7b8fab3dee844b65eba7f15c409ce6cecc4ad47f
Vaidehi Venkatesan <vvenkate@vvenkate-deb7-64>
2014-09-11 20:14:18 -0400

    LinkAnalysis

--- matlab/linkAnalysis.m
+++ matlab/linkAnalysis.m
@@ -1,46 +1,72 @@
-function linkAnalysis(threshold)
+function linkAnalysis(thresholds)
     cuisines = {'indian', 'chinese', 'mexican', 'spanish', 'italian', 'french'};
-    edgeWts = zeros(threshold*6, 1);
-    srcs = cell(threshold*6, 1);
-    dests = cell(threshold*6, 1);
-    k = 0;
-    for i=1:6
-        edgWtFile = strcat(cuisines{i} , '_cooc_wtDist.csv');
-        [src, dest, wt] =  loadFile(edgWtFile);
-        [wt, orderedIndices] = sort(wt, 'descend');
-        subsetIndices = orderedIndices(1:threshold);
-        a = src(subsetIndices);
-        b = dest(subsetIndices);
-        srcs(k+1:k+threshold, 1) = a;
-        dests(k+1:k+threshold, 1) = b;
-        edgeWts(k+1:k+threshold, 1) = wt(1:threshold);
-        k = k+threshold;
+    
+    thresholdContainer = containers.Map(int32(0), ?handle);
+    
+    xvalues = zeros(numel(thresholds,1));
+    yvalues = zeros(numel(thresholds,1));
+    
+    thresholdContainerPerCuisine = containers.Map(int32(0), ?handle);
+    
+    for i1 = 1:numel(thresholds)
+        threshold = thresholds(i1);
+        edgeWts = zeros(threshold*6, 1);
+        srcs = cell(threshold*6, 1);
+        dests = cell(threshold*6, 1);
+        
+        cuisineContainer = containers.Map(char('a'), ?handle);
+        
+        k = 0;
+        for i=1:6
+            
+            edgWtFile = strcat(cuisines{i} , '_cooc_wtDist.csv');
+            [src, dest, wt] =  loadFile(edgWtFile);
+            [wt, orderedIndices] = sort(wt, 'descend');
+            subsetIndices = orderedIndices(1:threshold);
+            a = src(subsetIndices);
+            b = dest(subsetIndices);
+            srcs(k+1:k+threshold, 1) = a;
+            dests(k+1:k+threshold, 1) = b;
+            edgeWts(k+1:k+threshold, 1) = wt(1:threshold);
+            k = k+threshold;
+            
+            cuisineWiseIngredContainer = containers.Map(char('a'), int32(0));
+            addToContainer(a, cuisineWiseIngredContainer);
+            addToContainer(b, cuisineWiseIngredContainer);
+            cuisineContainer(cuisines{i}) = cuisineWiseIngredContainer;
+                        
+        end
+        
+        thresholdContainerPerCuisine(threshold) = cuisineContainer;
+        allData = {srcs dests edgeWts};
+        ingredContainer = containers.Map(char('a'), int32(0));
+        addToContainer(srcs, ingredContainer);
+        addToContainer(dests, ingredContainer);
+        ingreds = ingredContainer.keys;
+        ingredFreq = zeros(numel(ingreds, 1));
+        for i=1:numel(ingreds)
+            count = ingredContainer(ingreds{i});
+            ingredFreq(i) = count;
+        end
+        [ingredFreq, orderedIndices] = sort(ingredFreq, 'descend');
+        sortedIngreds = ingreds(orderedIndices);
+        thresholdContainer(threshold) = {ingredFreq, sortedIngreds, allData};
+        xvalues(i1) = threshold;
+        yvalues(i1) = length(ingredFreq);
     end
-    allData = {srcs dests edgeWts};
-    ingredContainer = containers.Map(char('a'), int32(0));
-    for i=1:numel(srcs)
-        if ingredContainer.isKey(srcs{i})
-            count = ingredContainer(srcs{i});
-            ingredContainer(srcs{i}) = count + 1;
+    save('IngredientsGrowthWithLinks.mat', 'thresholdContainer', 'thresholdContainerPerCuisine');
+    figure;
+    plot(xvalues, yvalues, '.');
+    figure;
+    loglog(xvalues, yvalues, '.');
+end
+function addToContainer(data, container)
+    for i=1:numel(data)
+        if container.isKey(data{i})
+            count = container(data{i});
+            container(data{i}) = count + 1;
         else
-            ingredContainer(srcs{i}) = 1;
+            container(data{i}) = 1;
         end
     end
-    for i=1:numel(dests)
-        if ingredContainer.isKey(dests{i})
-            count = ingredContainer(dests{i});
-            ingredContainer(dests{i}) = count + 1;
-        else
-            ingredContainer(dests{i}) = 1;
-        end
-    end
-    ingreds = ingredContainer.keys;
-    ingredFreq = zeros(numel(ingreds));
-    for i=1:numel(ingreds)
-        count = ingredContainer(ingreds{i});
-        ingredFreq(i) = count;
-    end
-    [ingredFreq, orderedIndices] = sort(ingredFreq, 'descend');
-    sortedIngreds = ingreds(orderedIndices);
-    disp a;
 end
---
+++ matlab/perCuisineLinkAnalysis.m
@@ -0,0 +1,39 @@
+function perCuisineLinkAnalysis(thresholds)
+load IngredientsGrowthWithLinks.mat
+cuisines = {'indian', 'chinese', 'mexican', 'spanish', 'italian', 'french'};
+yvalues = zeros(length(thresholds),6);
+for i=1:numel(thresholds)
+    threshold = thresholds(i);
+    perThresholdContainer = thresholdContainerPerCuisine(threshold);
+    
+    for i1=1:numel(cuisines)
+        ingreds = perThresholdContainer(cuisines{i1});
+        yvalues(threshold, i1) = length(ingreds);
+    end
+end
+
+colorIndex = [1, 8, 25, 40, 56, 64]; 
+c = colormap(jet);
+
+figure;
+for i=1:6
+    plot(yvalues(:,i), '.', 'Color', c(colorIndex(i), :));
+    hold on;
+end
+legend(cuisines);
+xlabel('links');
+ylabel('ingredients per cuisine');
+title('Growth of ingredient count in networks with links per cuisine');
+
+
+figure;
+for i=1:6
+    loglog(yvalues(:,i), '.', 'Color', c(colorIndex(i), :));
+    hold on;
+end
+legend(cuisines);
+xlabel('links');
+ylabel('ingredients per cuisine');
+title('loglog - Growth of ingredient count in networks with links per cuisine');
+
+end
